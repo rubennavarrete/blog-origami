2e42d0c8710d8ebe22fe88cfc83ad21ac956d02e
Dario Navarrete <rd_navarrete@outlook.com>
2021-11-06 17:32:37 -0500

    ☄️ bug reparado

--- components/Podcast.js
+++ components/Podcast.js
@@ -1,14 +1,5 @@
 export const Podcast = ({ episode }) => {
     return (
-        <iframe
-            src={`https://open.spotify.com/embed/episode/${episode}`}
-            width="100%"
-            height="232"
-            frameborder="0"
-            allowTransparency="true"
-            allow="encrypted-media"
-        >
-
-        </iframe>
+        <h1>Hola Tu</h1>
     )
 }
