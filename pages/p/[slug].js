3367b9550632f2b23aa060906c9c2a868e136f8f
Dario Navarrete <rd_navarrete@outlook.com>
2022-02-14 11:56:32 -0500

    Carga de Juego

--- pages/p/[slug].js
+++ pages/p/[slug].js
@@ -7,6 +7,26 @@ export default function Page({ source, frontmatter }) {
   return (
     <Layout type="page" metadata={frontmatter}>
       <MDXRemote {...source} components={MDXComponents} />
+      <div
+        id="html_embed_widget_11018"
+        className="html_embed_widget embed_wrapper"
+      >
+        <div data-height="600" data-width="960" class="game_frame game_loaded">
+          <iframe
+            mozallowfullscreen="true"
+            allow="autoplay; fullscreen; geolocation; microphone; camera; midi"
+            src="/Museo-Origami-Juego-Html/index.html"
+            msallowfullscreen="true"
+            scrolling="no"
+            allowfullscreen="true"
+            webkitallowfullscreen="true"
+            id="game_drop"
+            allowtransparency="true"
+            frameborder="0"
+            style={{ width: "100%", height: "70vh" }}
+          ></iframe>
+        </div>
+      </div>
     </Layout>
   );
 }
